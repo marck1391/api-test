8251f01f6b812ba04f9ea487f3e2d1b0c1722629
Marco Orta <marck1391@gmail.com>
2020-02-16 23:42:19 -0600

    remove vulnerabilities

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {Router} from 'aws-lambda-ts'
+import { Router } from 'aws-lambda-ts'
 import { App } from './app'
 
-let router: Router = Reflect.getMetadata('router', App)
+let router:Router = Reflect.getMetadata('router', App)
 
-export async function handler(event){
+export async function handler(event) {
     return router.call(event)
 }
